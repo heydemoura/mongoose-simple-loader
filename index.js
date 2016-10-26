2f0f2c7113ba62bd0eac014efa8ce0ddbcbcf022
Heyde Moura <moura.heyde@gmail.com>
2016-10-25 22:54:15 -0300

    Initial release with basic functionallity

---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var path = require('path');
+var mongoose = require('mongoose');
+
+// Object that will be exported
+var db = function(dir) {
+	this.dir = dir;	
+
+	this.models = {};
+
+	var	_files = fs.readdirSync(dir);
+	for (var file in _files) {
+		if(_files[file][0] !== '.') {
+			var module = _files[file].substr(0, _files[file].length-3);
+			this.models[module] = require(dir + '/' + module);
+		}
+	}
+
+	this.connect = function(uri, options) {
+		return mongoose.connect(uri, options);
+	}
+
+	return this;
+};
+
+module.exports = db;
